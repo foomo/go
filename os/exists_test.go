22ed5aadcf05b2090bf87e51c8c64d1ed1881188
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2023-08-02 15:28:51 +0200

    fix(os): use custom env var

--- os/exists_test.go
+++ os/exists_test.go
@@ -1,23 +1,31 @@
 package os_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
-	"github.com/foomo/go/os"
+	osx "github.com/foomo/go/os"
 )
 
 func TestExists(t *testing.T) {
-	assert.True(t, os.Exists("GOROOT"))
-	assert.False(t, os.Exists("UNDEFINED"))
+	require.NoError(t, os.Unsetenv("FOO"))
+	assert.False(t, osx.Exists("FOO"))
+
+	require.NoError(t, os.Setenv("FOO", "bar"))
+	assert.True(t, osx.Exists("FOO"))
 }
 
 func TestMustExists(t *testing.T) {
+	require.NoError(t, os.Unsetenv("FOO"))
 	assert.Panics(t, func() {
-		os.MustExists("UNDEFINED")
+		osx.MustExists("FOO")
 	})
+
+	require.NoError(t, os.Setenv("FOO", "bar"))
 	assert.NotPanics(t, func() {
-		os.Exists("GOROOT")
+		osx.Exists("FOO")
 	})
 }
