fade107dac856ed46e9470c3b1534d3642e87974
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-04-16 08:48:18 +0200

    chore: update setup

--- os/env_test.go
+++ os/env_test.go
@@ -31,55 +31,55 @@ func TestMustEnvExists(t *testing.T) {
 }
 
 func TestGetenv(t *testing.T) {
-	_ = os.Unsetenv("FOO")
+	t.Setenv("FOO", "")
 	assert.Equal(t, "foo", osx.Getenv("FOO", "foo"))
-	_ = os.Setenv("FOO", "bar")
+	t.Setenv("FOO", "bar")
 	assert.Equal(t, "bar", osx.Getenv("FOO", "foo"))
 }
 
 func TestGetenvBool(t *testing.T) {
-	_ = os.Unsetenv("FOO")
+	t.Setenv("FOO", "")
 	if v, err := osx.GetenvBool("FOO", false); assert.NoError(t, err) {
-		assert.Equal(t, false, v)
+		assert.False(t, v)
 	}
 
-	_ = os.Setenv("FOO", "true")
+	t.Setenv("FOO", "true")
 	if v, err := osx.GetenvBool("FOO", false); assert.NoError(t, err) {
-		assert.Equal(t, true, v)
+		assert.True(t, v)
 	}
 }
 
 func TestGetenvInt64(t *testing.T) {
-	_ = os.Unsetenv("FOO")
+	t.Setenv("FOO", "")
 	if v, err := osx.GetenvInt64("FOO", 1); assert.NoError(t, err) {
 		assert.Equal(t, int64(1), v)
 	}
 
-	_ = os.Setenv("FOO", "2")
+	t.Setenv("FOO", "2")
 	if v, err := osx.GetenvInt64("FOO", 1); assert.NoError(t, err) {
 		assert.Equal(t, int64(2), v)
 	}
 }
 
 func TestGetenvFloat64(t *testing.T) {
-	_ = os.Unsetenv("FOO")
+	t.Setenv("FOO", "")
 	if v, err := osx.GetenvFloat64("FOO", 0.1); assert.NoError(t, err) {
 		assert.Equal(t, 0.1, v)
 	}
 
-	_ = os.Setenv("FOO", "0.2")
+	t.Setenv("FOO", "0.2")
 	if v, err := osx.GetenvFloat64("FOO", 0.1); assert.NoError(t, err) {
 		assert.Equal(t, 0.2, v)
 	}
 }
 
 func TestGetenvStringSlice(t *testing.T) {
-	_ = os.Unsetenv("FOO")
+	t.Setenv("FOO", "")
 	assert.Nil(t, osx.GetenvStringSlice("FOO", nil))
 
-	_ = os.Setenv("FOO", "foo")
+	t.Setenv("FOO", "foo")
 	assert.Equal(t, []string{"foo"}, osx.GetenvStringSlice("FOO", nil))
 
-	_ = os.Setenv("FOO", "foo,bar")
+	t.Setenv("FOO", "foo,bar")
 	assert.Equal(t, []string{"foo", "bar"}, osx.GetenvStringSlice("FOO", nil))
 }
