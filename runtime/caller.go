a90c7a4b7c647717284bbbc3dc0f2898ddf2f3e8
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-07-31 10:13:30 +0200

    fix(runtime): resolve inline caller

--- runtime/caller.go
+++ runtime/caller.go
@@ -8,12 +8,17 @@ import (
 
 // Caller returns caller information for the function skip frames up the call stack.
 func Caller(skip int) (shortName, fullName, file string, line int, ok bool) { //nolint:nonamedreturns
-	pc, file, line, ok := stdruntime.Caller(skip + 1)
-	if !ok {
+	var pcs [1]uintptr
+	if stdruntime.Callers(skip+2, pcs[:]) == 0 {
 		return "unknown", "Unknown", "unknown", 0, false
 	}
 
-	fullName = stdruntime.FuncForPC(pc).Name()
+	fr, _ := stdruntime.CallersFrames(pcs[:]).Next()
+	if fr.Function == "" {
+		return "unknown", "Unknown", "unknown", 0, false
+	}
+
+	fullName, file, line = fr.Function, fr.File, fr.Line
 
 	dirname, filename := path.Split(file)
 	file = path.Join(path.Base(dirname), filename)
